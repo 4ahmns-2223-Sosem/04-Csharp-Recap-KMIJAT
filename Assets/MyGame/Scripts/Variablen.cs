d81e8afc327b73b8e93afaadfe22ebc8b905600b
KMijat <kristina.mijatovic04@htl-salzburg.ac.at>
2023-03-23 09:48:15 +0100

    Variablen

---
+++ Assets/MyGame/Scripts/Variablen.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variablen : MonoBehaviour
+{
+    public int  Haus = 22;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Haus);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
